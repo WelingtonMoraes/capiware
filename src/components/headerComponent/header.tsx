18997b7c892ed01beac09338fc80ba4d96cc4180
welington moraes <welington_moraes@econeteditora.com.br>
2023-09-06 17:05:18 -0300

    header

---
+++ src/components/headerComponent/header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type HeaderProps = {
+	title: string;
+};
+
+const Header: React.FC<HeaderProps> = ({title}) => (
+	<header>
+		<p>teste porra</p>
+	</header>
+);
+
+export default Header;
