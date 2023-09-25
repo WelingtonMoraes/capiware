b2bb356f5a52971d8c9d5a71befc582378cca011
Welington Moraes <53630451+WelingtonMoraes@users.noreply.github.com>
2023-09-24 21:07:01 -0300

    novo siteeee

--- src/components/headerComponent/header.tsx
+++ src/components/headerComponent/header.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Typewriter from 'typewriter-effect';
 import './style.scss';
 import ButtonPrimary from '../button/ButtonPrimary';
 import logoEmpresa from '../../assets/logos/logo_empresa.png';
@@ -7,29 +8,30 @@ const Header: React.FC = () => (
 	<header>
 		<div className='bd-content'>
 			<nav className='navbar' role='navigation' aria-label='main navigation'>
-				<div className='navbar-brand'>
+				<div className='navbar-start'>
 					<a className='navbar-item' href=''>
 						<img src={logoEmpresa} alt='' />
-					</a>
-
-					<a role='button' className='navbar-burger' aria-label='menu' aria-expanded='false' data-target='navbarBasicExample'>
-						<span aria-hidden='true'></span>
-						<span aria-hidden='true'></span>
-						<span aria-hidden='true'></span>
+						<Typewriter
+							options={{
+								strings: ['Capi', 'Software', 'Capiware'],
+								autoStart: true,
+								loop: true,
+							}}
+						/>
 					</a>
 				</div>
 
-				<div id='navbarBasicExample' className='navbar-menu'>
-					<div className='navbar-start'>
-						<a className='navbar-item'>Home</a>
-						<a className='navbar-item'>Documentation</a>
+				<div id='navbarBasicExample' className='navbar-end'>
+					<div className='navbar-menu'>
+						<a className='navbar-item'>Sobre nós</a>
+						<a className='navbar-item'>Contato</a>
 					</div>
 
-					<div className='navbar-end'>
+					{/* <div className='navbar-end'>
 						<div className='navbar-item'>
 							<ButtonPrimary text={'Let’s Talk'}/>
 						</div>
-					</div>
+					</div> */}
 				</div>
 			</nav>
 		</div>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,8 +5,22 @@ import '../styles/main/App.scss';
 function App() {
 	return (
 		<div className='App'>
-			<div className='banner'>
-
+			<div className='banner is-flex is-align-items-center'>
+				<div className='bd-content'>
+					<div className='columns'>
+						<div className='column is-three-fifths'></div>
+						<div className='column'>
+							<h1>A sua <b>visão</b>,<br/> o nosso <b>código</b>.</h1>
+							<p>
+								Se você está procurando soluções de software inovadoras,
+								confiáveis e personalizadas para impulsionar o seu negócio,
+								você veio ao lugar certo. Na <b>Capiware</b>, somos apaixonados por
+								criar sistemas de software de <b>alta qualidade</b> que atendam às
+								necessidades específicas de cada cliente.
+							</p>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
