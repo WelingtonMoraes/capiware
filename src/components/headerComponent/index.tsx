374d1f002421d5837a1d029d0d6d69acb614311e
welington moraes <welington_moraes@econeteditora.com.br>
2023-10-03 16:07:12 -0300

    mobile

--- src/components/headerComponent/index.tsx
+++ src/components/headerComponent/index.tsx
@@ -1,41 +1,53 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import Typewriter from 'typewriter-effect';
 import './style.scss';
-import ButtonPrimary from '../button';
 import logoEmpresa from '../../assets/logos/logo_empresa.png';
 
-const Header: React.FC = () => (
-	<header>
-		<div className='bd-content'>
-			<nav className='navbar' role='navigation' aria-label='main navigation'>
-				<div className='navbar-start'>
-					<a className='navbar-item' href=''>
-						<img src={logoEmpresa} alt='' />
-						<Typewriter
-							options={{
-								strings: ['Capi', 'Software', 'Capiware'],
-								autoStart: true,
-								loop: true,
-							}}
-						/>
-					</a>
-				</div>
+const Header: React.FC = () => {
+	const [menuAberto, setMenuAberto] = useState(false);
 
-				<div id='navbarBasicExample' className='navbar-end'>
-					<div className='navbar-menu'>
-						<a href='#idQuemSomos' className='navbar-item'>Sobre nós</a>
-						<a href='#idContatos' className='navbar-item'>Contato</a>
+	const toggleMenu = () => {
+		setMenuAberto(!menuAberto);
+	};
+
+	return (
+		<header>
+			<div className='bd-content'>
+				<nav className='navbar' role='navigation' aria-label='main navigation'>
+					<div className='navbar-start'>
+						<a className='navbar-item' href=''>
+							<img src={logoEmpresa} alt='' />
+							<Typewriter
+								options={{
+									strings: ['Capi', 'Software', 'Capiware'],
+									autoStart: true,
+									loop: true,
+								}}
+							/>
+						</a>
 					</div>
 
-					{/* <div className='navbar-end'>
-						<div className='navbar-item'>
-							<ButtonPrimary text={'Let’s Talk'}/>
+					<div id='navbarBasicExample' className='navbar-end menu'>
+						<div className='navbar-menu'>
+							<a href='#idQuemSomos' className='navbar-item'>Sobre nós</a>
+							<a href='#idContatos' className='navbar-item'>Contato</a>
 						</div>
-					</div> */}
-				</div>
-			</nav>
-		</div>
-	</header>
-);
+
+						<div className={`menu-list ${menuAberto ? 'is-open' : ''}`}>
+							<a href='#idQuemSomos' className='navbar-item'>Sobre nós</a>
+							<a href='#idContatos' className='navbar-item'>Contato</a>
+						</div>
+
+						<div className={`navbar-burger burger ${menuAberto ? 'is-active' : ''}`} onClick={toggleMenu}>
+							<span></span>
+							<span></span>
+							<span></span>
+						</div>
+					</div>
+				</nav>
+			</div>
+		</header>
+	);
+};
 
 export default Header;
